1bc61b37fa9065a2b8337b1fcb03d39e5b2103ac
mahdiafzal <mahdiafzal@gmail.com>
2017-06-18 13:57:17 +0430

    update last changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,10 @@
-$(function() {
+$(function () {
     $('.itemmatchHeight, .item').matchHeight();
+    $('.search-again-filter_select2').select2({
+        language: {
+            noResults: function (params) {
+                return "موردی یافت نشد.";
+            }
+        }
+    });
 });
